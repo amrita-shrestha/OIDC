eb9542f75040597d56808e34721848cba271f9a7
amrita <bunnycrestha34@gmail.com>
2023-06-29 14:54:08 +0545

    Play with different ocis param

--- src/oidc.php
+++ src/oidc.php
@@ -2,18 +2,18 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Jumbojett\OpenIDConnectClient;
-use Facile\OpenIDClient\Client\ClientBuilder;
-use Facile\OpenIDClient\Issuer\IssuerBuilder;
 
 $oidc = new OpenIDConnectClient('https://localhost:9200',
     'xdXOt13JKxym1B1QcEncf2XDkLAexMBFwiT9j6EfhhHFJhs2KM9jbjTmf8JBXE69',
     'UBntmLjC2yYCeHwsyj73Uwo9TAaecAetRwMw0xYcvNL9yRdLSUi0hUAHfvCHFeFh'
 );
 
-$oidc->setRedirectURL('http://localhost/ProfessionalDevelopment/PHPOIDC/src/redirect.html');
-$oidc->addScope(["email", "profile"]);
+$oidc->setRedirectURL('http://localhost/ProfessionalDevelopment/PHPOIDC/src/welcome.php');
+$oidc->addScope(["offline_access"]);
 $oidc->setCodeChallengeMethod('S256');
+$oidc->setResponseTypes(['token']);
 $oidc->addAuthParam(['response_mode' => 'query']);
+$oidc->setTokenEndpointAuthMethodsSupported(["client_secret_basic"]);
 
 // Disable SSL certificate verification
 // Required for local servers using https
@@ -25,7 +25,7 @@ try {
 } catch(Exception $e){
     echo $e;
 }
-$token = $oidc->getAccessToken();
+$token = $oidc->getTokenResponse();
 echo $token;
 
 // Client
--- src/welcome.php
+++ src/welcome.php
@@ -3,8 +3,16 @@
 <body>
 Welcome
 <?php
+global $token;
     $params = parse_url($_SERVER[REQUEST_URI]);
-    echo $params["query"];
+    parse_str($params["query"], $output);
+    echo "<pre>";
+    var_dump($output);
+    echo $output['access_token'];
+echo "</pre>";
+echo "hello";
+echo $token;
+
 ?>
 
 
