c85a1b86059986e4e5e74eeab084f050817bc116
amrita <bunnycrestha34@gmail.com>
2023-07-28 09:37:06 +0545

    Make pratice

--- src/welcome2.php
+++ src/welcome2.php
@@ -4,12 +4,12 @@
 Welcome
 <?php
 var_dump($_REQUEST);
-//    $params = parse_url($_SERVER[REQUEST_URI]);
-//    parse_str($params["query"], $output);
-//    echo "<pre>";
-//    var_dump($output);
-//    echo $output['access_token'];
-//echo "</pre>";
+    $params = parse_url($_SERVER[REQUEST_URI]);
+    parse_str($params["query"], $output);
+    echo "<pre>";
+    var_dump($output);
+    echo $output['access_token'];
+echo "</pre>";
 ?>
 
 
